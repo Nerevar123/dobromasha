11665034b69db824351dc56b8323d5766863736d
Menachem Neiman <nerevar123@gmail.com>
2023-03-01 03:20:40 +0200

    add google tag

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import ViteRadar from 'vite-plugin-radar';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    ViteRadar({
+      analytics: {
+        id: 'G-RE1H714GFT',
+      },
+    }),
+  ],
   base: '.',
 });
