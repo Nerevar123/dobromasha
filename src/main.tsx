791984f187a2ca2484614a2085c10dfde57d0c91
Menachem Neiman <ext-menachem.neiman@placer.io>
2024-05-24 12:37:15 +0300

    add speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <StyleSheetManager disableVendorPrefixes={process.env.NODE_ENV === 'development'}>
       <RouterProvider router={router} />
       <Analytics />
+      <SpeedInsights />
     </StyleSheetManager>
   </React.StrictMode>,
 );
